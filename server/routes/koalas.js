8bc41bb360f6323c2b11dc28a587a78fe45684b8
Jen Show <jennifer.show@gmail.com>
2017-08-11 11:35:39 -0500

    added koala data to DOM

--- server/routes/koalas.js
+++ server/routes/koalas.js
@@ -5,6 +5,22 @@ var pool = require ('../modules/pool');
 
 router.get('/', function(req, res){
     console.log('hit get route')
+    pool.connect(function(errorConnectingToDatabase, client, done){
+        if(errorConnectingToDatabase){
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query('SELECT * FROM koalas;', function(errorMakingQuery, result){
+                done();
+                if(errorMakingQuery) {
+                    console.log('Error making database query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                }
+            })
+        }
+    })
 });
 
 router.post('/', function(req, res){
